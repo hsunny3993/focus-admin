5281aa28847dcd901cf2d0809462afd969d19f82
hsunny3993 <hsunny3993@gmail.com>
2023-07-07 16:58:49 +0200

    - add assets manifest

--- lib/focusadmin/engine.rb
+++ lib/focusadmin/engine.rb
@@ -1,5 +1,9 @@
 module Focusadmin
   class Engine < ::Rails::Engine
     isolate_namespace Focusadmin
+
+    initializer "focusadmin.assets.precompile" do |app|
+      app.config.assets.precompile += %w( focusadmin/*.js focusadmin/*.scss )
+    end
   end
 end
