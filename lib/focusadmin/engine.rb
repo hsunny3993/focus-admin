861117f0b6681d9de123afd21de5027085211caf
hsunny3993 <hsunny3993@gmail.com>
2023-07-09 20:04:39 +0200

    - upgrade assets pipeline

--- lib/focusadmin/engine.rb
+++ lib/focusadmin/engine.rb
@@ -1,9 +1,5 @@
 module Focusadmin
   class Engine < ::Rails::Engine
     isolate_namespace Focusadmin
-
-    initializer "focusadmin.assets.precompile" do |app|
-      app.config.assets.precompile += %w( focusadmin/* focusadmin/* )
-    end
   end
 end
